2b1c22808ff1a0db0cc4b9d493009d5254676486
ThomasMelancon48 <42358755+ThomasMelancon48@users.noreply.github.com>
2018-08-28 13:44:00 -0500

    Add files via upload

---
+++ ArrayHomework/Source.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+//Exercise - Arrays
+//int matrix [5] does not work.  It does not work because the 
+//value only goes to 5.
+//int collection does not work because the value has to be greater than 0.
+
+//1.
+int main()
+{
+	int number[10] = { 0, 0, 1, 0, 0, 1, 0, 0, 1, 1 };
+	int matrix[5] = { 1, 2, 3, 4, 5 };
+	double radii[10] = { (3.2, 4.7) };
+	int table[7] = { 2, 27, 45, 39 };
+	int collection[-20];
+	char codes[] = { 'A', 'X', '1', '2', 's' };
+	int hours[3] = { 8, 12, 16 };
+
+	//2.
+	int values[] = { 2, 6, 10, 14 };
+	cout << values[2];
+	cout << ++values[0];
+	cout << values [1]++;
+	int x = 2;
+	cout << values[++x];
+	cout << values[4];
+
+	//3.
+	int i;
+	int data[10];
+	for (int i = 10; i > 0; i);
+	{data[i] = i;
+	cout << data[i] << endl;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		cout << data[i] << endl;
+	}
+
+	//4.
+	int number[5];
+	int i;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Enter a number:";
+		cin >> x;
+		number[x];
+	}
+	for (int i = 5; i > 0; i)
+	{
+		cout << number[i];
+	}
+
+	//5.
+
+	int Numb;
+	int i;
+	int max = a[0];
+	int min = a[0];
+	cout << "Enter a value";
+	for (i = 0; i < 10; i)
+	{
+		if i) > max;
+	};
+	else if i < min;
+	cout << max << endl;
+	cout << min << endl;
+
+	//6.
+
+
+
+
+
+	//7.
+
+
+
+
+
+	//8.
+
+
+
+
+
+
+
+	//9.
+
+
+
+
+
+	system("pause");
+	return 0;
+}
